ac4d3d9a5e820bd23e73438201c37c4803a0d6d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 16:44:27 -0300

    Saved grace/main.py at Timestamp: 2018-04-17 16:43:31

--- grace/main.py
+++ grace/main.py
@@ -2,10 +2,11 @@
 from _spy.vitollino.main import Cena, Elemento
 
 linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by_elevation_world-d3e2cex.png"
-linkElemento = "http://icons.iconarchive.com/icons/custom-icon-design/mono-general-4/512/eye-icon.png"
 
 class Cena2():
   def __init__(self):
+    linkOlho = "http://icons.iconarchive.com/icons/custom-icon-design/mono-general-4/512/eye-icon.png"
+
     self.cena = Cena(img = linkIMG)
     olho = Elemento(img = linkOlho, style = dict(top = '400', left = '400'))
 
@@ -15,3 +16,4 @@ class Cena2():
     self.cena.esquerda = cena
   def connectDireita(self, cena):
     self.cena.direita = cena
+    
