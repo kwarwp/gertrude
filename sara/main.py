036930bfdfa3ef0e61cbb2c5976cdc45434e373a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 11:06:47 -0300

    Created sara/main.py at Timestamp: 2018-04-17 14:06:47

---
+++ sara/main.py
@@ -0,0 +1,9 @@
+# gertrude.sara.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto 
+
+linkBackdropCena2 = "http://s2.glbimg.com/yX02WqYevO4ETQxDkX8ReUFLfKk=/s.glbimg.com/jo/g1/f/original/2014/12/22/onibus-nathalia.jpg"
+
+def mockBackdropCena2():
+  mbcdrop = bcdrop(img = linkBackdropCena2)
+  
+mockBackdropCena2()
