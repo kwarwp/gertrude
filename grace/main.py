b86592fa9ecb3e4a564a72829a79133e64307996
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 16:35:49 -0300

    Saved grace/main.py at Timestamp: 2018-04-17 16:34:52

--- grace/main.py
+++ grace/main.py
@@ -5,14 +5,11 @@ linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by
 class Cena2():
   def __init__(self):
     self.cena = Cena(img = linkIMG)
-    self.cenaEsquerda = Cena()
-    self.cenaDireita = Cena()
-    self.cena.direita = self.cenaDireita
-    self.cena.esquerda = self.cenaEsquerda
+
 
   def vai(self, *_):
     self.cena.vai()    
   def connectEsquerda(self, cena):
-    self.cenaEsquerda = cena
+    self.cena.esquerda = cena
   def connectDireita(self, cena):
-    self.cenaDireita = cena
+    self.cena.direita = cena
