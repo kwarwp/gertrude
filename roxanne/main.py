277b2aba47b346bc49301fd4c1d632d8a3964329
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 11:11:57 -0300

    Saved roxanne/main.py at Timestamp: 2018-04-17 14:11:56

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,1 +1,15 @@
 # gertrude.roxanne.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+
+linkCena = "https://vignette.wikia.nocookie.net/bitstrips/images/a/ab/Bathroom.png/revision/latest?cb=20121224051149"
+
+def mockCena():
+  mCena = Cena(img = linkCena)
+  mCena.vai()
+mockCena()
+  
+  
+  
+  
+  
+  
