38405d050fa69673b335156720e1ce76f75f86a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 15:50:53 -0300

    Saved meredith/main.py at Timestamp: 2018-04-17 15:49:56

--- meredith/main.py
+++ meredith/main.py
@@ -6,11 +6,14 @@ linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by
 class Cena2():
 
   def __init__(self):
-    from anastasia.main import Cena1
     self.cena2 = Cena(img = linkIMG)
-    cena1 = Cena1()
+    cena1 = Cena()
+    cena1.vai = self.cena1Vai
     self.cena2.esquerda = cena1
 
-  def vai(self, *_):
+  def vai(self, *_):    
     self.cena2.vai()
+  def cena1Vai(self):
+    from anastasia.main import Cena1
+    Cena1().vai
   
