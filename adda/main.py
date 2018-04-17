70becd8d3c5fef84cfbb9ee7ebd1701a6bdfd9c6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 16:21:18 -0300

    Saved adda/main.py at Timestamp: 2018-04-17 16:20:22

--- adda/main.py
+++ adda/main.py
@@ -4,8 +4,6 @@ from danae.main import Cena1
 from grace.main import Cena2
 
 cena1 = Cena1()
-#cena2 = Cena2()
-#cena1.connectDireita(cena1)
-#cena2.connectEsquerda(cena2)
-
-Cena2().vai()
+cena2 = Cena2()
+cena1.connectDireita(cena1)
+cena2.connectEsquerda(cena2)
