adc47e65265aa35f28a7e9e2614849e3a210045d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 15:20:53 -0300

    Saved meredith/main.py at Timestamp: 2018-04-17 18:20:53

--- meredith/main.py
+++ meredith/main.py
@@ -1,8 +1,13 @@
 # gertrude.meredith.main.py
 from _spy.vitollino.main import Cena
+from anastasia.main import Cena1
 
 linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by_elevation_world-d3e2cex.png"
 
-def Cena1():
-  cena1 = Cena(img = linkIMG)
-  cena1.vai()
+def Cena2():
+  cena2 = Cena(img = linkIMG)
+  cena1MOCK = Cena()
+  cena2.esquerda = cena1MOCK
+  cena1MOCK.vai = Cena1
+  
+  cena2.vai()
