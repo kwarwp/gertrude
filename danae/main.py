deac9a73fd4e1cee5018d705d8be22f0388580af
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 16:35:53 -0300

    Saved danae/main.py at Timestamp: 2018-04-17 16:34:57

--- danae/main.py
+++ danae/main.py
@@ -2,18 +2,14 @@
 from _spy.vitollino.main import Cena
 
 linkIMG = "https://openclipart.org/image/800px/svg_to_png/232279/MandelbrotFrame.png"
-
 class Cena1():
   def __init__(self):
     self.cena = Cena(img = linkIMG)
-    cenaEsquerda = Cena()
-    cenaDireita = Cena()
-    self.cena.direita = cenaDireita
     self.cena.esquerda = cenaEsquerda
 
   def vai(self, *_):
     self.cena.vai() 
-  def conectEsquerda(self, cena):
+  def connectEsquerda(self, cena):
     self.cena.esquerda = cena
   def connectDireita(self, cena):
     self.cena.direita = cena
