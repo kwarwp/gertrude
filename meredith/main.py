1033b01897135f340008022b98226aacc1511de0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 15:03:21 -0300

    Created meredith/main.py at Timestamp: 2018-04-17 18:03:21

---
+++ meredith/main.py
@@ -0,0 +1,8 @@
+# gertrude.meredith.main.py
+from _spy.vitollino.main import Cena
+
+linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by_elevation_world-d3e2cex.png"
+
+def Cena1():
+  cena1 = Cena(img = linkIMG)
+  cena1.vai()
