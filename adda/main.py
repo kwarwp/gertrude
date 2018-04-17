6f3d25868409c1d2b2ead84aeb3211da37f92c9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 16:20:17 -0300

    Created adda/main.py at Timestamp: 2018-04-17 16:19:21

---
+++ adda/main.py
@@ -0,0 +1,11 @@
+# gertrude.adda.main.py
+
+from danae.main import Cena1
+from grace.main import Cena2
+
+cena1 = Cena1()
+#cena2 = Cena2()
+#cena1.connectDireita(cena1)
+#cena2.connectEsquerda(cena2)
+
+Cena2().vai()
