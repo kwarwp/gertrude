53d3b2b997e8a078cec7079eb8d73a60cdcba2d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 11:13:40 -0300

    Saved sara/main.py at Timestamp: 2018-04-17 14:13:40

--- sara/main.py
+++ sara/main.py
@@ -4,7 +4,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 linkBackdropCena2 = "http://s2.glbimg.com/yX02WqYevO4ETQxDkX8ReUFLfKk=/s.glbimg.com/jo/g1/f/original/2014/12/22/onibus-nathalia.jpg"
 
 def mockBackdropCena2():
-  mbcdrop = bcdrop(img = linkBackdropCena2)
-  mbcdrop.vai()
-mockbcdrop()
+  mmockBackdropCena2 = BackdropCena2(img = linkBackdropCena2)
+  mmockBackdropCena2.vai()
+mockmockBackdropCena2()
   
