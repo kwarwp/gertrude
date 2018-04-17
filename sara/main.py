0485b44107f209dfce6accc751496b70c97748d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 11:49:32 -0300

    Saved sara/main.py at Timestamp: 2018-04-17 14:49:32

--- sara/main.py
+++ sara/main.py
@@ -3,8 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 linkbackdropcena2 = "http://s2.glbimg.com/yX02WqYevO4ETQxDkX8ReUFLfKk=/s.glbimg.com/jo/g1/f/original/2014/12/22/onibus-nathalia.jpg"
 linkpersonagem = "https://vignette.wikia.nocookie.net/cartoonnetwork/images/4/48/Ad%C3%A3o_Le%C3%A3o_%281%29.png/revision/latest?cb=20150125151129&path-prefix=pt-br"
-linkcemiterio = "https://thumbs.dreamstime.com/z/cen%C3%A1rio-de-halloween-com-cemit%C3%A9rio-1-20146311.jpg
-
+linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
 def backdropcena2():
   	cenaponto = Cena (img = linkbackdropcena2)  
   	personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
