668b4842f3a556a079125945d747ac0774f11342
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 11:30:50 -0300

    Saved sara/main.py at Timestamp: 2018-04-17 14:30:50

--- sara/main.py
+++ sara/main.py
@@ -2,11 +2,13 @@
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
 linkbackdropcena2 = "http://s2.glbimg.com/yX02WqYevO4ETQxDkX8ReUFLfKk=/s.glbimg.com/jo/g1/f/original/2014/12/22/onibus-nathalia.jpg"
+linkpersonagem = "https://vignette.wikia.nocookie.net/cartoonnetwork/images/4/48/Ad%C3%A3o_Le%C3%A3o_%281%29.png/revision/latest?cb=20150125151129&path-prefix=pt-br"
 
-
-def cackdropcena2():
+def backdropcena2():
   	cenaponto = Cena (img = linkbackdropcena2)
   	cenaponto.vai()
+  	personagem = garotinho (img = linkpersonagem)
+  	personagem.entra(cenaponto)
 backdropcena2()
   
   
