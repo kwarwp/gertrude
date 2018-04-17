43a97323c91cd97b69748264179ed3f5b70d7a80
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-17 16:23:07 -0300

    Created grace/main.py at Timestamp: 2018-04-17 16:22:11

---
+++ grace/main.py
@@ -0,0 +1,18 @@
+# gertrude.grace.main.py
+from _spy.vitollino.main import Cena
+
+linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by_elevation_world-d3e2cex.png"
+class Cena2():
+  def __init__(self):
+    self.cena = Cena(img = linkIMG)
+    cenaEsquerda = Cena()
+    cenaDireita = Cena()
+    self.cena.direita = cenaDireita
+    self.cena.esquerda = cenaEsquerda
+
+  def vai(self, *_):
+    self.cena.vai()    
+  def connectEsquerda(self, cena):
+    self.cena.esquerda = cena
+  def connectDireita(self, cena):
+    self.cena.direita = cena
