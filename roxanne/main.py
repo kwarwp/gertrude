47bb2216ae2120a3f6b33cfd9a3643b241258f0c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-24 10:36:35 -0300

    Saved roxanne/main.py at Timestamp: 2018-04-24 13:36:34

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,10 +1,16 @@
 # gertrude.roxanne.main.py
-from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 
-linkCena = "https://vignette.wikia.nocookie.net/bitstrips/images/a/ab/Bathroom.png/revision/latest?cb=20121224051149"
+w = 300
+STYLE['width'] = w
+STYLE['height'] = w
+linkCena = ""
+linkObjeto = "https://applets.imgix.net/https%3A%2F%2Fassets.ifttt.com%2Fimages%2Fchannels%2F891279159%2Ficons%2Fon_color_large.png%3Fversion%3D0?ixlib=rails-2.1.3&w=240&h=240&auto=compress&s=8e9316a345ccb3bf6569068e4e025d23"
 
 def mockCena():
   mCena = Cena(img = linkCena)
+  mObj = Elemento(img = linkObjeto)
+  mObj.entra(mCena, style = dict(width = 30, bottom = 200))
   mCena.vai()
 mockCena()
   
