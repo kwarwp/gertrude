d13b506ed3372111bc46a243caf212b061ec9630
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-24 10:54:17 -0300

    Saved sara/main.py at Timestamp: 2018-04-24 13:54:16

--- sara/main.py
+++ sara/main.py
@@ -1,18 +1,12 @@
 # gertrude.sara.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto 
-
+#Cena2
 linkbackdropcena2 = "http://s2.glbimg.com/yX02WqYevO4ETQxDkX8ReUFLfKk=/s.glbimg.com/jo/g1/f/original/2014/12/22/onibus-nathalia.jpg"
 linkpersonagem = "https://vignette.wikia.nocookie.net/cartoonnetwork/images/4/48/Ad%C3%A3o_Le%C3%A3o_%281%29.png/revision/latest?cb=20150125151129&path-prefix=pt-br"
 linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
 personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
-
-
+'''
 def backdropcena2():
-  	from _spy.vitollino.main import Cena, Elemento, Texto 
-  	linkbackdropcena2 = "http://s2.glbimg.com/yX02WqYevO4ETQxDkX8ReUFLfKk=/s.glbimg.com/jo/g1/f/original/2014/12/22/onibus-nathalia.jpg"
-  	linkpersonagem = "https://vignette.wikia.nocookie.net/cartoonnetwork/images/4/48/Ad%C3%A3o_Le%C3%A3o_%281%29.png/revision/latest?cb=20150125151129&path-prefix=pt-br"
-  	linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
-  	personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
   	cenaponto = Cena (img = linkbackdropcena2)  
   	personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
   	personagem.entra(cenaponto)
@@ -26,7 +20,30 @@ def backdropcena2():
   	gameover = Texto(cenacemiterio, "Se ferrou!")
   	gameover.vai()
     
-
-   
-    
 backdropcena2()
+'''
+class  Cena2():
+	def __init__(self):
+		from julia.main import Cena1
+		self.cena = Cena(img = linkbackdropcena2)
+		cena1 = Cena1()
+  		personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
+  		personagem.entra(self.cena)
+  		txtpersonagem = Texto(self.cena, "e agora?")
+  		personagem.vai = txtpersonagem.vai
+  		self.cena.vai()
+  		cenacemiterio = Cena (img = linkcemiterio)
+  		self.cena.direita = cenacemiterio
+  		personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
+  		personagem_falecido.entra(cenacemiterio)
+  		gameover = Texto(cenacemiterio, "Se ferrou!")
+  		gameover.vai()
+  		self.cena.esquerda = cena1
+	def vai(self, *_):
+		self.cena.vai()
+
+c = Cena2()
+c.vai()
+    
+    
+    
