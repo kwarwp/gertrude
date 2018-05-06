19e8f19928343a6aea28b7c279741ced28781cf5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-06 18:18:30 -0300

    Saved amanda/main.py at Timestamp: 2018-05-06 21:18:30

--- amanda/main.py
+++ amanda/main.py
@@ -1,1 +1,44 @@
 # gertrude.amanda.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto 
+#from julia.main import Cena1
+#Cena2
+linkbackdropcena2 = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
+linkpersonagem = "https://vignette.wikia.nocookie.net/cartoonnetwork/images/4/48/Ad%C3%A3o_Le%C3%A3o_%281%29.png/revision/latest?cb=20150125151129&path-prefix=pt-br"
+linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
+personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
+'''
+def backdropcena2():
+  	cenaponto = Cena (img = linkbackdropcena2)  
+  	personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
+  	personagem.entra(cenaponto)
+  	txtpersonagem = Texto(cenaponto, "e agora?")
+  	personagem.vai = txtpersonagem.vai
+  	cenaponto.vai()
+  	cenacemiterio = Cena (img = linkcemiterio)
+  	cenaponto.direita = cenacemiterio
+  	personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
+  	personagem_falecido.entra(cenacemiterio)
+  	gameover = Texto(cenacemiterio, "Se ferrou!")
+  	gameover.vai()
+    
+backdropcena2()
+'''
+class Cena2():
+	def __init__(self):
+		self.cena = Cena(img = linkbackdropcena2)
+		#cena1 = Cena1()
+  		personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
+  		personagem.entra(self.cena)
+  		txtpersonagem = Texto(self.cena, "e agora?")
+  		personagem.vai = txtpersonagem.vai
+  		cenacemiterio = Cena (img = linkcemiterio)
+  		self.cena.direita = cenacemiterio
+  		personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
+  		personagem_falecido.entra(cenacemiterio)
+  		gameover = Texto(cenacemiterio, "Se ferrou!")
+  		gameover.vai()
+  		#self.cena.esquerda = cena1
+	def vai(self, *_):
+		self.cena.vai()
+
+Cena2().vai()
