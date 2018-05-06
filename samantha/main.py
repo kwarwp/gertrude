291830a69862e7d5b2b7c54c872fceb4090e9cdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-06 02:11:18 -0300

    Saved samantha/main.py at Timestamp: 2018-05-06 05:11:18

--- samantha/main.py
+++ samantha/main.py
@@ -7,4 +7,38 @@ linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/
 personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
 
 def backdropcena2():
-  	cenaponto = Cena (img = linkbackdropcena2,tit="universo", style = dict(left=500, top=500, width=100, height=100)) 
+  	cenaponto = Cena (img = linkbackdropcena2) 
+  	personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 100, top=200, width=200, height=200))
+  	personagem.entra(cenaponto)
+  	txtpersonagem = Texto(cenaponto, "e agora?")
+  	personagem.vai = txtpersonagem.vai
+  	cenaponto.vai()
+  	cenacemiterio = Cena (img = linkcemiterio)
+  	cenaponto.direita = cenacemiterio
+  	personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=300,bottom=20))
+  	personagem_falecido.entra(cenacemiterio)
+  	gameover = Texto(cenacemiterio, "Se ferrou!")
+  	gameover.vai()
+    
+backdropcena2()
+
+class Cena2():
+	def __init__(self):   
+ 		cenaponto = Cena (img = linkbackdropcena2, tit="universo", style = dict(left=500, top=500, width=100, height=100))     
+		self.cena = Cena(img = linkbackdropcena2)
+		#cena1 = Cena1()
+  		personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 50, top=200, width=300, height=200))
+  		personagem.entra(self.cena)
+  		txtpersonagem = Texto(self.cena, "e agora?")
+  		personagem.vai = txtpersonagem.vai
+  		cenacemiterio = Cena (img = linkcemiterio)
+  		self.cena.direita = cenacemiterio
+  		personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
+  		personagem_falecido.entra(cenacemiterio)
+  		gameover = Texto(cenacemiterio, "Se ferrou!")
+  		gameover.vai()
+  		#self.cena.esquerda = cena1
+	def vai(self, *_):
+		self.cena.vai()
+
+Cena2().vai()
