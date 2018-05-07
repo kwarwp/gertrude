a80954cbfb4e13ab551a8a96aed21aa39d8d4dd1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-07 20:20:59 -0300

    Saved julia/main.py at Timestamp: 2018-05-07 23:20:59

--- julia/main.py
+++ julia/main.py
@@ -5,11 +5,12 @@ from sara.main import *
 #Cena 1
 IMG = {
        'home' : 'http://kafgw.com/wp-content/uploads/inside-houses-canterbury-college-university-windsor_1337735.jpg',
-       'hand' : 'https://media-minecraftforum.cursecdn.com/attachments/133/64/635733362039447280.png',
+       'personagem' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_2_9367638513.png?disp=inline',
        'umbrella' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_3_9501450609.png?disp=inline',
        'casaco' : 'https://www.achildsplace.org/wp-content/uploads/2014/10/rain-coat-clip-art-raincoat-300x247.png',
        'cellphone' : 'https://vignette.wikia.nocookie.net/uncyclopedia/images/6/6b/Cellphone.png/revision/latest?cb=20100105203250',
-       'window' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/oie_transparent.png?disp=inline'
+       'window' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/oie_transparent.png?disp=inline',
+       'lampada' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_7202278580.png?disp=inline'
        }
 
 class Cena1:
@@ -20,14 +21,16 @@ class Cena1:
   		self.__casaco = Elemento(img=IMG['casaco'], trt="Casaco", style=dict(top=150, left=20, bottom=20, height=150, width=70))
   		self.__phone = Elemento(img=IMG['cellphone'], trt="Telefone", style=dict(top=180, left=180, bottom=20, width=30))
   		self.__window = Elemento(img=IMG['window'], trt="Janela", style=dict(top=-15, left=60, bottom=10, width=200))
-  		self.__hand = Elemento(img=IMG['hand'], trt="MÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ£os", style=dict(top=200, left=125, height=100, width=70))
-  		#txt = Texto(mCena, "CoÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©"
+  		self.__lampada = Elemento(img=IMG['lampada'], trt="Lampada", style=dict(top=0, left=130, bottom=10, width=50))
+  		self.__personagem = Elemento(img=IMG['personagem'], trt="personagem", style=dict(top=100,left=70,bottom=0, width=120))
+  		#txt = Texto(mCena, "CoÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©"
   		self.__umbrella.entra(self.__cena1)
   		self.__casaco.entra(self.__cena1)
   		self.__phone.entra(self.__cena1)
   		self.__window.entra(self.__cena1)
-  		self.__hand.entra(self.__cena1)
-  		self.__hand.vai = self.teste
+  		self.__personagem.entra(self.__cena1)
+  		self.__lampada.entra(self.__cena1)
+  		self.__personagem.vai = self.teste
   		self.__cena1.direita = cena2;
 	def vai(self): self.__cena1.vai();
 	def teste(self): print('Foi');
