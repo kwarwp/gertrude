a812f1bae47ff7c29ec3518462a8e4842d999e20
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-06 22:11:12 -0300

    Saved amanda/main.py at Timestamp: 2018-05-07 01:11:12

--- amanda/main.py
+++ amanda/main.py
@@ -2,16 +2,16 @@
 from _spy.vitollino.main import Cena, Elemento, Texto 
 #from julia.main import Cena1
 #Cena2
-linkbackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
+linkbackdropcena2 = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
 linkpersonagem = "https://dzy0ia.bn.files.1drv.com/y4mCpj--VWLV60iEJ-pMxcWj5qNmD4ADaUOa7--_3dCPywaXTvOJR7Swwev4gRzWeiKVPbGVm-hXYdZf4eLAnDy3jVcdztw1YRxwkrauYHHHssyFk3fBbjJQzZyal2cDRc6U0bQOj2TqfbMnibtNl5WjDt1T5WfApi0AWrm9HgZoSvELsvVsNE4BRRHcTpMxsUq0FA8RJjPi0YAHMD9Aa3Hyw/aaaaaaaaaaaa.png?psid=1"
 linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
 personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
 '''
-def backdropcena():
-  	cenaponto = Cena (img = linkbackdropcena)  
+def backdropcena2():
+  	cenaponto = Cena (img = linkbackdropcena2)  
   	personagem = Elemento (img = linkpersonagem)
   	personagem.entra(cenaponto)
-  	txtpersonagem = Texto(cenaponto, "Olá, eu sou a ideia do bem e do mal.Esse é um jogo de escolhas, sobre a breve história da humanidade.Para prosseguir clique no universo")
+  	txtpersonagem = Texto(cenaponto, "e agora?")
   	personagem.vai = txtpersonagem.vai
   	cenaponto.vai()
   	cenacemiterio = Cena (img = linkcemiterio)
@@ -25,11 +25,11 @@ backdropcena2()
 '''
 class Cena2():
 	def __init__(self):
-		self.cena = Cena(img = linkbackdropcena)
+		self.cena = Cena(img = linkbackdropcena2)
 		#cena1 = Cena1()
   		personagem = Elemento (img = linkpersonagem)
   		personagem.entra(self.cena)
-  		txtpersonagem = Texto(self.cena, "Olá, eu sou a ideia do bem e do mal.Esse é um jogo de escolhas, sobre a breve história da humanidade.Para prosseguir clique no universo")
+  		txtpersonagem = Texto(self.cena, "e agora?")
   		personagem.vai = txtpersonagem.vai
   		cenacemiterio = Cena (img = linkcemiterio)
   		self.cena.direita = cenacemiterio
