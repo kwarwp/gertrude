afce3a55ade8a4e6ac4650481ca28b49f9826350
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-08 17:51:58 -0300

    Saved samantha/main.py at Timestamp: 2018-05-08 20:51:58

--- samantha/main.py
+++ samantha/main.py
@@ -1,41 +1,40 @@
-# gertrude.samantha.main.pyfrom _spy.vitollino.main import Cena, Elemento, Texto 
+from _spy.vitollino.main import Cena, Elemento, Texto 
 #from julia.main import Cena1
-#Cena
-linkbackdropcena2 = "https://cdn.spacetelescope.org/archives/images/screen/heic0814a.jpg"
-linkpersonagem = "https://vignette.wikia.nocookie.net/cartoonnetwork/images/4/48/Ad%C3%A3o_Le%C3%A3o_%281%29.png/revision/latest?cb=20150125151129&path-prefix=pt-br"
-linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
-personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
-
-def backdropcena2():
-  	cenaponto = Cena (img = linkbackdropcena2) 
-  	personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 100, top=200, width=200, height=200))
+#Cena2
+linkbackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
+linkmadoka = "https://images.puella-magi.net/c/ca/Madoka_main_page.png?20110119201909"
+linkportasparauniversos = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
+gatocosmico = "http://i.imgur.com/RgaBeG7.png"
+'''
+def backdropcena():
+  	cenaponto = Cena (img = linkbackdropcena)  
+  	madoka = Elemento (img = linkpersonagem, tit="madoka")
   	personagem.entra(cenaponto)
-  	txtpersonagem = Texto(cenaponto, "e agora?")
+  	txtpersonagem = Texto(cenaponto, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
   	personagem.vai = txtpersonagem.vai
   	cenaponto.vai()
   	cenacemiterio = Cena (img = linkcemiterio)
-  	cenaponto.direita = cenacemiterio
-  	personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=300,bottom=20))
+  	cenaponto.direita = cenaponto
+  	personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
   	personagem_falecido.entra(cenacemiterio)
   	gameover = Texto(cenacemiterio, "Se ferrou!")
   	gameover.vai()
     
 backdropcena2()
-
+'''
 class Cena2():
-	def __init__(self):   
- 		cenaponto = Cena (img = linkbackdropcena2, tit="universo", style = dict(left=500, top=500, width=100, height=100))     
-		self.cena = Cena(img = linkbackdropcena2)
+	def __init__(self):
+		self.cena = Cena(img = linkbackdropcena)
 		#cena1 = Cena1()
-  		personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 50, top=200, width=300, height=200))
-  		personagem.entra(self.cena)
-  		txtpersonagem = Texto(self.cena, "e agora?")
-  		personagem.vai = txtpersonagem.vai
-  		cenacemiterio = Cena (img = linkcemiterio)
-  		self.cena.direita = cenacemiterio
-  		personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
-  		personagem_falecido.entra(cenacemiterio)
-  		gameover = Texto(cenacemiterio, "Se ferrou!")
+  		madoka = Elemento (img = linkmadoka, tit="madoka", style = dict(right= 100, top=145, width= 115,bottom=20))
+  		madoka.entra(self.cena)
+  		txtmadoka = Texto(self.cena, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
+  		madoka.vai = txtmadoka.vai
+  		cenaponto = Cena (img = linkbackdropcena)
+  		self.cena.meio = cenaponto
+  		personagem_falecido = Elemento (img = gatocosmico, tit="Socorro!", style = dict(right= 100, top=200, width=75,bottom=50))
+  		#personagem_falecido.entra(cenaponto)
+  		gameover = Texto(cenaponto, "O Hubble Ultra Deep Field é uma imagem de uma pequena região do espaço,na constelação de Fornax, composta por dados do Telescópio Espacial Hubble no período de 3 de setembro de 2003 a 16 de janeiro de 2004. É a imagem mais profunda do universo tirada em luz visível, ilustrando o universo tal como ele era há 13 bilhões de anos atrás.")
   		gameover.vai()
   		#self.cena.esquerda = cena1
 	def vai(self, *_):
