63c8517dc6d08220aa9ae07032bdd774b72bc70d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-07 21:44:34 -0300

    Saved julia/main.py at Timestamp: 2018-05-08 00:44:33

--- julia/main.py
+++ julia/main.py
@@ -10,7 +10,10 @@ IMG = {
        'casaco' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/cena1_capachuva.png?disp=inline',
        'cellphone' : 'https://vignette.wikia.nocookie.net/uncyclopedia/images/6/6b/Cellphone.png/revision/latest?cb=20100105203250',
        'window' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/oie_transparent.png?disp=inline',
-       'lampada' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_7202278580.png?disp=inline'
+       'lampada' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_7202278580.png?disp=inline',
+       'oculos': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_9286555293.png?disp=inline',
+       'bola' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_2_9661334197.png?disp=inline'
+       
        }
 
 class Cena1:
@@ -23,13 +26,15 @@ class Cena1:
   		self.__window = Elemento(img=IMG['window'], trt="Janela", style=dict(top=-15, left=60, bottom=10, width=200))
   		self.__lampada = Elemento(img=IMG['lampada'], trt="Lampada", style=dict(top=0, left=130, bottom=10, width=50))
   		self.__personagem = Elemento(img=IMG['personagem'], trt="personagem", style=dict(top=100,left=70,bottom=0, width=120))
-  		#txt = Texto(mCena, "CoÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©"
+  		self.__bola = Elemento(img=IMG['bola'], trt="bola", style=dict(top=220, left=0, bottom=10, width=70))
+        #txt = Texto(mCena, "CoÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©"
   		self.__umbrella.entra(self.__cena1)
   		self.__casaco.entra(self.__cena1)
   		self.__phone.entra(self.__cena1)
   		self.__window.entra(self.__cena1)
   		self.__personagem.entra(self.__cena1)
   		self.__lampada.entra(self.__cena1)
+  		self.__bola.entra(self.__cena1)
   		self.__personagem.vai = self.teste
   		self.__cena1.direita = cena2;
 	def vai(self): self.__cena1.vai();
