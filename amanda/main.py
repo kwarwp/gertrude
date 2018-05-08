9125ca693bfc6f45769eb4f9105a58951687bc0a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-08 15:51:01 -0300

    Saved amanda/main.py at Timestamp: 2018-05-08 18:51:00

--- amanda/main.py
+++ amanda/main.py
@@ -2,15 +2,15 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 #from julia.main import Cena1
 #Cena2
 linkbackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
-linkpersonagem = "https://images.puella-magi.net/c/ca/Madoka_main_page.png?20110119201909"
+linkmadoka = "https://images.puella-magi.net/c/ca/Madoka_main_page.png?20110119201909"
 linkportasparauniversos = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
 personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
 '''
 def backdropcena():
   	cenaponto = Cena (img = linkbackdropcena)  
-  	personagem = Elemento (img = linkpersonagem)
+  	madoka = Elemento (img = linkpersonagem, tit="madoka")
   	personagem.entra(cenaponto)
-  	txtpersonagem = Texto(cenaponto, "OlÃ¡, eu sou a ideia do bem e do mal.Esse Ã© um jogo de escolhas, sobre a breve histÃ³ria da humanidade.Para prosseguir clique no universo")
+  	txtpersonagem = Texto(cenaponto, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
   	personagem.vai = txtpersonagem.vai
   	cenaponto.vai()
   	cenacemiterio = Cena (img = linkcemiterio)
@@ -26,10 +26,10 @@ class Cena2():
 	def __init__(self):
 		self.cena = Cena(img = linkbackdropcena)
 		#cena1 = Cena1()
-  		personagem = Elemento (img = linkpersonagem)
-  		personagem.entra(self.cena)
-  		txtpersonagem = Texto(self.cena, "OlÃ¡, eu sou a ideia do bem e do mal.Esse Ã© um jogo de escolhas, sobre a breve histÃ³ria da humanidade.Para prosseguir clique no universo")
-  		personagem.vai = txtpersonagem.vai
+  		madoka = Elemento (img = linkmadoka, tit="madoka")
+  		madoka.entra(self.cena)
+  		txtpersonagem = Texto(self.cena, "Ola¡, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
+  		madoka.vai = txtpersonagem.vai
   		cenaponto = Cena (img = linkbackdropcena)
   		self.cena.direita = cenaponto
   		personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
