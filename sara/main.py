b31adbf374fc99613c40e8820563df5e1da62e78
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-08 00:10:04 -0300

    Saved sara/main.py at Timestamp: 2018-05-08 03:10:04

--- sara/main.py
+++ sara/main.py
@@ -6,23 +6,7 @@ linkbackdropcena2 = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/cena
 linkpersonagem = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_2_9367638513.png?disp=inline"
 linkcemiterio = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
 personagem_morto = "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_1_2944272726.png?disp=inline"
-'''
-def backdropcena2():
-  	cenaponto = Cena (img = linkbackdropcena2)  
-  	personagem = Elemento (img = linkpersonagem, tit="garotinho", style = dict(left= 150, top=100, width=60, height=200))
-  	personagem.entra(cenaponto)
-  	txtpersonagem = Texto(cenaponto, "e agora?")
-  	personagem.vai = txtpersonagem.vai
-  	cenaponto.vai()
-  	cenacemiterio = Cena (img = linkcemiterio)
-  	cenaponto.direita = cenacemiterio
-  	personagem_falecido = Elemento (img = personagem_morto, tit="morreu", style = dict(right= 100, top=200, width=125,bottom=20))
-  	personagem_falecido.entra(cenacemiterio)
-  	gameover = Texto(cenacemiterio, "Se ferrou!")
-  	gameover.vai()
-    
-backdropcena2()
-'''
+
 class Cena2():
 	def __init__(self,escolha=False):
 		self.cena = Cena(img = linkbackdropcena2)  
@@ -37,7 +21,7 @@ class Cena2():
   		personagem_falecido.entra(cenacemiterio)
   		gameover = Texto(cenacemiterio, "Se ferrou!")
   		gameover.vai()
-		#Lógica
+		#Logica
 		if escolha: pass#self.cena.direita = #sucesso;
 		else: self.cena.direita = cenacemiterio;
   		self.cena.esquerda = Cena1()
