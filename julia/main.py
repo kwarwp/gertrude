1c1f700eb82cd9972a2fb50d92f2987b4c46bc20
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-08 01:06:00 -0300

    Saved julia/main.py at Timestamp: 2018-05-08 04:05:59

--- julia/main.py
+++ julia/main.py
@@ -19,23 +19,14 @@ IMG = {
 class Cena1:
 	def __init__(self):
   		self.__cena1 = Cena(img=IMG['home']);
-		c = Cena2(True)
-  		umbrella = Elemento(img=IMG['umbrella'], trt="Guarda Chuva", style=dict(top=100, left=170, bottom=20, height=150, width=70))
-  		casaco = Elemento(img=IMG['casaco'], trt="Casaco", style=dict(top=150, left=220, bottom=20, height=150, width=70))
-  		phone = Elemento(img=IMG['cellphone'], trt="Telefone", style=dict(top=180, left=180, bottom=20, width=30))
-  		window = Elemento(img=IMG['window'], trt="Janela", style=dict(top=-15, left=60, bottom=10, width=200))
-  		lampada = Elemento(img=IMG['lampada'], trt="Lampada", style=dict(top=0, left=130, bottom=10, width=50))
-  		personagem = Elemento(img=IMG['personagem'], trt="personagem", style=dict(top=100,left=70,bottom=0, width=120))
-  		bola = Elemento(img=IMG['bola'], trt="bola", style=dict(top=220, left=0, bottom=10, width=70))
-  		oculos = Elemento(img=IMG['oculos'], vai=c, trt="Oculos", style=dict(top=160, left=20,bottom=20, width=50))
-  		umbrella.entra(self.__cena1);
-  		casaco.entra(self.__cena1);
-  		phone.entra(self.__cena1);
-  		window.entra(self.__cena1);
-  		personagem.entra(self.__cena1);
-  		lampada.entra(self.__cena1);
-  		bola.entra(self.__cena1);
-  		oculos.entra(self.__cena1);
+  		umbrella = Elemento(img=IMG['umbrella'], cena=self.__cena1, vai=Cena2(True), trt="Guarda Chuva", style=dict(top=100, left=170, bottom=20, height=150, width=70))
+  		casaco = Elemento(img=IMG['casaco'], cena=self.__cena1, vai=Cena2(True), trt="Casaco", style=dict(top=150, left=220, bottom=20, height=150, width=70))
+  		phone = Elemento(img=IMG['cellphone'], cena=self.__cena1, vai=Cena2(False), trt="Telefone", style=dict(top=180, left=180, bottom=20, width=30))
+  		window = Elemento(img=IMG['window'], cena=self.__cena1, vai=Cena2(False), trt="Janela", style=dict(top=-15, left=60, bottom=10, width=200))
+  		lampada = Elemento(img=IMG['lampada'], cena=self.__cena1, vai=Cena2(False), trt="Lampada", style=dict(top=0, left=130, bottom=10, width=50))
+  		personagem = Elemento(img=IMG['personagem'], cena=self.__cena1, trt="personagem", style=dict(top=100,left=70,bottom=0, width=120))
+  		bola = Elemento(img=IMG['bola'], cena=self.__cena1, trt="bola", vai=Cena2(False), style=dict(top=220, left=0, bottom=10, width=70))
+  		oculos = Elemento(img=IMG['oculos'], cena=self.__cena1, vai=Cena2(False), trt="Oculos", style=dict(top=160, left=20,bottom=20, width=50))
   		self.__cena1.esquerda = Cena2(False);
 		texto = Texto(self.__cena1,'Voce deve escolher entre os objetos da direita e os da esquerda');
 		texto.vai()
