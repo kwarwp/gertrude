b1f1996e1e3a6785870ce2f3431c9486ca4ab3a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-22 11:25:11 -0300

    Saved samantha/main.py at Timestamp: 2018-05-22 14:25:11

--- samantha/main.py
+++ samantha/main.py
@@ -15,7 +15,7 @@ def backdropcena():
   	txtpersonagem = Texto(cenaponto, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
   	personagem.vai = txtpersonagem.vai
   	cenaponto.vai()
-  	cenadeepspace = Cena (img = linkdeepspace)
+  	cenaponto = Cena (img = linkdeepspace)
   	cenamarilyn.direita = cenadeepspace
   	cenadois = Texto(cenadeepspace, "O Hubble Ultra Deep Field uma imagem de uma pequena regiao do espaço,na constelaçao de Fornax, composta por dados do Telescopio Espacial Hubble, 3 de setembro de 2003 a 16 de janeiro de 2004, a imagem mais profunda do universo tirada em luz visivel, ilustrando o universo tal como ele era a 13 bilhoeses de anos atras.")
   	cenadois.vai()
