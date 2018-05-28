f0395c6eab60a0fe1314cb269f1c93666ff5cc84
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-28 13:54:53 -0300

    Saved naomi/main.py at Timestamp: 2018-05-28 16:54:53

--- naomi/main.py
+++ naomi/main.py
@@ -21,11 +21,11 @@ class Cena1:
 			if p == True:
 				falaMario = Texto(self.__cena1,"It's-a me, Mario!");
 				falaMario.vai();
-				personagem = False;                
+				p = False;                
 			else:
 				falaSonic = Texto(self.__cena1,'Entao vc prefere O Mario');
 				falaSonic.vai();
-				personagem = True;
+				p = True;
 				
 		self.__mario = Elemento(img=IMG['mario'], vai=trade, cena=self.__cena1, trt="Mario", style=dict(top=100, left=20, bottom=20, height=150, width=70))
 		self.__sonic = Elemento(img=IMG['sonic'], vai=trade, cena=self.__cena1, trt="Sonic", style=dict(top=100, left=200, bottom=20, height=150, width=70))
