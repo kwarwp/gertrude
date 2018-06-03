6f8071f3a277c4f864e652f0f504a2c40c13975c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-02 23:47:11 -0300

    Saved ruzwana/main.py at Timestamp: 2018-06-03 02:47:10

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,6 +1,6 @@
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-linkbackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
+linkBackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
 linkmarilyn = "https://activufrj.nce.ufrj.br/file/pedropeclat/WhatsApp_Image_2018-05-22_at_11.54.54.jpeg?disp=inline"
 linkdeepspace = "https://www.google.com.br/search?q=deep+field&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjy2bbPu5nbAhXMhZAKHREpDnUQ_AUICigB&biw=1366&bih=662#imgrc=Ba3E_PaLJ3QRrM:"
 linkmerlin = "https://activufrj.nce.ufrj.br/file/pedropeclat/00merlin-the-sword-in-the-stone-packing.png?disp=inline"
@@ -16,22 +16,12 @@ linkbehelit ="https://vignette.wikia.nocookie.net/evil/images/3/3f/The_Behelit.j
 linkend ="https://ia.media-imdb.com/images/M/MV5BNWM0OWRlNzctZDU5NS00ZDAzLWFkY2EtNDUxNjAxMjQ1ZWM4L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTY3Mzc2OTY@._V1_SX1777_CR0,0,1777,999_AL_.jpg"
 linkgatocosmico = "http://i.imgur.com/RgaBeG7.png"
 
+def historia():
+    galaxia = cena(img = linkBackdropcena)
+    marilyn = elemento(img = linkmarilyn,tit="marilyn")
+    marilyn.entra(galaxia)
+    txtmarilyn = texto(galaxia,"Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
+    marilyn.vai=txtmarilyn.vai
+    galaxia.vai
 
-
-class Cena1():
-	def __init__(self):
-		self.cena = Cena(img = linkbackdropcena)
-		#cena1 = Cena1()
-  		marilyn = Elemento (img = linkmarilyn, tit="marilyn", style = dict(right= 100, top=145, width= 115,bottom=20))
-  		marilyn.entra(self.cena)
-  		txtmarilyn = Texto(self.cena, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
-  		marilyn.vai = txtmarilyn.vai
-  		cenadeepspace = Cena (img = linkdeepspace)
-  		self.cena.direita = cenadeepspace
-  		cenadois = Texto(cenadeepspace, "O Hubble Ultra Deep Field uma imagem de uma pequena regiao do espaaÃÂÃÂÃÂÃÂ§o,na constelaÃÂÃÂÃÂÃÂ§ao de Fornax, composta por dados do Telescopio Espacial Hubble, 3 de setembro de 2003 a 16 de janeiro de 2004, a imagem mais profunda do universo tirada em luz visivel, ilustrando o universo tal como ele era a 13 bilhoeses de anos atras.")
-  		cenadois.vai()
-  		#self.cena.esquerda = cena1
-	def vai(self, *_):
-		self.cena.vai()
-
-Cena1().vai()
+historia()
