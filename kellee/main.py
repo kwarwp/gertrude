2e7ef7335e1a1aca3d565892b2090e0214769e1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-04 01:20:29 -0300

    Saved kellee/main.py at Timestamp: 2018-06-04 04:20:28

--- kellee/main.py
+++ kellee/main.py
@@ -17,7 +17,7 @@ def Defumado():
     floresta = Cena(img=linkFloresta)
     Golem2 = Elemento(img = linkGolem, style =dict(top = 150, left = 120, height = 250, width = 100))
     Golem2.entra(floresta)
-    txtGolem2 = Texto(floresta, "Irei atrÃÂ¡s da caÃÂ§a para me alimentar, talvez na cidade!")
+    txtGolem2 = Texto(floresta, "Irei atrÃÂÃÂ¡s da caÃÂÃÂ§a para me alimentar, talvez na cidade!")
     Golem2.vai = txtGolem2.vai
     
     caverna.direita = floresta
@@ -28,7 +28,7 @@ def Defumado():
     berlim = Cena(img=linkBerlim)
     Golem3 = Elemento(img = linkGolem, style =dict(top = 150, left = 120, height = 250, width = 100))
     Golem3.entra(berlim)
-    txtGolem3 = Texto(berlim, "A cidade irÃÂ¡ ficar diante dos meus pÃÂ©s!")
+    txtGolem3 = Texto(berlim, "A cidade irÃÂÃÂ¡ ficar diante dos meus pÃÂÃÂ©s!")
     Golem3.vai = txtGolem3.vai
     
     floresta.direita = berlim
@@ -40,7 +40,7 @@ def Defumado():
     Mistica = Cena(img = linkMistica)
     Mago = Elemento(img = linkMago, tit="Mago", style=dict(left= 120, top= 150, width= 100, height= 250))
     Mago.entra(Mistica)
-    txtMago=Texto(Mistica, "Onde andas minha criaÃÂ§ÃÂ£o?")
+    txtMago=Texto(Mistica, "Onde andas minha criaÃÂÃÂ§ÃÂÃÂ£o?")
     Mago.vai = txtMago.vai
     
     berlim.direita = Mistica
@@ -51,7 +51,7 @@ def Defumado():
     berlim2 = Cena(img = linkBerlim)
     Amiguinho = Elemento(img = linkAmiguinho, style =dict(top = 150, left = 120, height = 250, width = 100))
     Amiguinho.entra(berlim2)
-    txtAmiguinho=Texto(berlim2, "Quem hÃÂ¡ de entrar em minha morada? Yo soy Dollynho!")
+    txtAmiguinho=Texto(berlim2, "Quem hÃÂÃÂ¡ de entrar em minha morada? Yo soy Dollynho!")
     Amiguinho.vai = txtAmiguinho.vai
     
     Mistica.direita = berlim2
@@ -59,7 +59,7 @@ def Defumado():
     
     berlim3 = Cena(img=linkBerlim)
     Golem4 = Elemento(img=linkGolem, style = dict( top = 150, left = 120, height = 250, width = 100))
-    txtGolem4 = Texto(berlim3, "Essa cidade ÃÂ© pequena demais pra nois dois, meu brodi!")
+    txtGolem4 = Texto(berlim3, "Essa cidade ÃÂÃÂ© pequena demais pra nois dois, meu brodi!")
     Golem4.entra(berlim3)
     Golem4.vai = txtGolem4.vai
     
@@ -71,7 +71,7 @@ def Defumado():
     Amiguinho2 = Elemento(img = linkAmiguinho, style =dict(top = 150, left = 20, width = 100, height = 250))
     Golem5.entra(berlim4)
     Amiguinho2.entra(berlim4)
-    txtAmiguinho2=Texto(berlim4, "Veremos ver quem irÃÂ¡ ficar com o territÃÂ³rio!")
+    txtAmiguinho2=Texto(berlim4, "Veremos ver quem irÃÂÃÂ¡ ficar com o territÃÂÃÂ³rio!")
     Amiguinho2.vai = txtAmiguinho2.vai
     
     berlim3.direita = berlim4
