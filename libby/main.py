fdd098f3747a677e1189f988d212a2b32108c8c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-17 22:33:54 -0300

    Saved libby/main.py at Timestamp: 2018-06-18 01:33:54

--- libby/main.py
+++ libby/main.py
@@ -1,4 +1,7 @@
 # gertrude.libby.main.py
-from _spy.vitollino.main import *
+# from _spy.vitollino.main import *
 from stacy.main import *
 
+p = Personagem(nome = 'teste', fala = 'Oi eu sou o Goku');
+#o = Objeto(nome = 'cadeira');
+c = Cena
