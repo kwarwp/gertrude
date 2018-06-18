69da697a1a761087ee5d3499eb266c84b28dda21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-17 22:20:25 -0300

    Saved stacy/main.py at Timestamp: 2018-06-18 01:20:25

--- stacy/main.py
+++ stacy/main.py
@@ -12,11 +12,18 @@ class Personagem:
 	
 	def getPersonagem(self): return self.__personagem;
 
+class Objeto:
+
+	def __init__(self, nome, posicao = dict(top=100, left=20, bottom=20, height=150, width=70), pontuacao, acao, img):
+		self.__objeto = Elemento(img = img, trt = nome, style = posicao, vai = acao);
+
+	def getObjeto(self): return self.__objeto;
+
 class Cena:
 
-	def __init__(self, background, score, elementos, personagens = []):
+	def __init__(self, background, score, objetos = [], personagens = []):
 		self.__cena = Cena(img = background);
 		for p in personagens: p.getPersonagem().entra(self.__cena);
-		for e in elementos : e.getElemento().entra(self.__cena);
+		for o in objetos : o.getObjeto().entra(self.__cena);
 
 	def vai(self): self.__cena.vai();
