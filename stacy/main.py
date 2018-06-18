91c9db0e8f31101da7f638eb2ac81a722cc0ca31
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-17 21:59:11 -0300

    Created stacy/main.py at Timestamp: 2018-06-18 00:59:11

---
+++ stacy/main.py
@@ -0,0 +1,14 @@
+# gertrude.stacy.main.py
+from _spy.vitollino.main import *
+
+class Personagem:
+
+	def __init__(self, nome = '', cena = '', posicao = dict(top=100, left=20, bottom=20, height=150, width=70), fala = '', img = ''):
+		if len(fala) == 0:
+			texto = Texto(cena, fala);
+			self.__personagem = Elemento(img = img, cena = cena, trt = nome, style = posicao, vai = texto.vai);
+		else:
+			self.__personagem = Elemento(img = img, cena = cena, trt = nome, style = posicao);
+	
+	def getPersonagem(self):
+		return self.__personagem;    
