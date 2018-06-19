7e0d832f482a94344db8d1330cd68704ab13fd45
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-19 14:39:29 -0300

    Saved anastasia/main.py at Timestamp: 2018-06-19 17:39:29

--- anastasia/main.py
+++ anastasia/main.py
@@ -2,38 +2,38 @@
 # Importando a biblioteca do Vitollino
 from _spy.vitollino.main import *
 
-# ConfiguraÃ§Ã£o da largura e altura da cena
+# Configuracao da largura e altura da cena
 STYLE["width"] = 1000
 STYLE["height"] = "600px"
 
-# Criando um dicionÃ¡rio com as imagens
+# Criando um dicionario com as imagens
 IMG = {
-       'background' : 'https://media-cdn.tripadvisor.com/media/photo-s/07/34/b1/37/parque-do-japao.jpg',
-       'moeda_piscante' : 'http://3.bp.blogspot.com/-xxHdoS90xwo/VR1jjtJUh1I/AAAAAAAAQAI/1Sw0A7IliUw/s1600/Coin%2B4.gif',
-       'moeda' : 'https://cdn.icon-icons.com/icons2/651/PNG/512/Icon_Business_Set_00003_A_icon-icons.com_59841.png',
-       'seta' : 'http://ap.imagensbrasil.org/images/PNG-Setadourada.png'
-       }
+	'background' : 'https://media-cdn.tripadvisor.com/media/photo-s/07/34/b1/37/parque-do-japao.jpg',
+	'moeda_piscante' : 'http://3.bp.blogspot.com/-xxHdoS90xwo/VR1jjtJUh1I/AAAAAAAAQAI/1Sw0A7IliUw/s1600/Coin%2B4.gif',
+	'moeda' : 'https://cdn.icon-icons.com/icons2/651/PNG/512/Icon_Business_Set_00003_A_icon-icons.com_59841.png',
+	'seta' : 'http://ap.imagensbrasil.org/images/PNG-Setadourada.png'
+}
 
 # Cria a classe da cena
 class Lago:
-	# ConstrÃ³i a cena com os objetos e as aÃ§Ãµes de cada objeto
+	# Construindo a cena com os objetos e as funcoes de cada objeto
 	def __init__(self):
 
-		# Inicia o inventÃ¡rio
+		# Inicia o inventario
 		INVENTARIO.inicia();
         
 		# Cria a cena
 		self.__lago = Cena(img = IMG['background']);
         
-		# Cria os objetos da cena juntamente com suas aÃ§Ãµes
+		# Cria os objetos da cena juntamente com suas funcoes
 		self.__moedaPiscante = Elemento(img=IMG['moeda_piscante'], vai=self.__vaiMoedaPiscante, cena=self.__lago, trt="Guarda Chuva", style=dict(top=100, left=170, bottom=20, height=150, width=70))
 		self.__moeda = Elemento(img=IMG['moeda'], vai=self.__vaiMoeda, cena=self.__lago, trt="Moeda Piscante", style=dict(top=150, left=220, bottom=20, height=150, width=70))
 		seta = Elemento(img=IMG['seta'], cena=self.__lago, trt="Seguir em frente", style=dict(top=80, left=160, bottom=20, width=30))
         
-	# Coloca a moeda piscante no inventÃ¡rio
+	# Coloca a moeda piscante no inventario
 	def __vaiMoedaPiscante(self, _=0): INVENTARIO.bota(self.__moedaPiscante)      
 
-	# Coloca a moeda normal no inventÃ¡rio
+	# Coloca a moeda normal no inventario
 	def __vaiMoeda(self, _=0): INVENTARIO.bota(self.__moeda)
 		#self.__moeda.vai = vaiMoeda
 		#self.__moeda.entra(self.__lago)
