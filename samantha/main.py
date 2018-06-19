744a0a6d4779b206db0f36937885bd6c2cc24614
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-19 11:54:56 -0300

    Saved samantha/main.py at Timestamp: 2018-06-19 14:54:56

--- samantha/main.py
+++ samantha/main.py
@@ -1,51 +1,25 @@
-from _spy.vitollino.main import Cena, Elemento, Texto 
-#from julia.main import Cena1
-#Cena2
-linkbackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
-linkmarilyn = "https://activufrj.nce.ufrj.br/file/pedropeclat/WhatsApp_Image_2018-05-22_at_11.54.54.jpeg?disp=inline"
-linkdeepspace = "https://www.google.com.br/search?q=deep+field&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjy2bbPu5nbAhXMhZAKHREpDnUQ_AUICigB&biw=1366&bih=662#imgrc=Ba3E_PaLJ3QRrM:"
-linmerlin = "https://activufrj.nce.ufrj.br/file/pedropeclat/00merlin-the-sword-in-the-stone-packing.png?disp=inline"
-linkportasparauniversos = "https://static.brusheezy.com/system/resources/premium_previews/000/041/258/non_2x/Eerie-Halloween-Wallpaper-PSD.jpg"
-linkporta1 = "https://activufrj.nce.ufrj.br/file/pedropeclat/Dore.jpg?disp=inline"
-#linkloba =
-#linkporta2 =
-linkmadoka = "https://images.puella-magi.net/c/ca/Madoka_main_page.png?20110119201909"
-#linkporta3 =
-#linkhipatia =
-#linkporta4 =
-linkbehelit ="https://vignette.wikia.nocookie.net/evil/images/3/3f/The_Behelit.jpg/revision/latest?cb=20151110185922"
-linkend ="https://ia.media-imdb.com/images/M/MV5BNWM0OWRlNzctZDU5NS00ZDAzLWFkY2EtNDUxNjAxMjQ1ZWM4L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTY3Mzc2OTY@._V1_SX1777_CR0,0,1777,999_AL_.jpg"
-linkgatocosmico = "http://i.imgur.com/RgaBeG7.png"
+from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
+from _spy.vitollino.main import INVENTARIO as inv
+
+from soraya.main import Bloco
+from browser import alert
 
 
-def backdropcena():
-  	cena1 = Cena (img = linkbackdropcena)  
-  	marilyn = Elemento (img = linkmarilyn, tit="marilyn")
-  	personagem.entra(cena1)
-  	txtpersonagem = Texto(cena1, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
-  	personagem.vai = txtpersonagem.vai
-  	cena1.vai()
-  	cena2 = Cena (img = linkdeepspace)
-  	cenamarilyn.direita = cenadeepspace
-  	cenadois = Texto(cenadeepspace, "O Hubble Ultra Deep Field uma imagem de uma pequena regiao do espaÃÂ§o,na constelaÃÂ§ao de Fornax, composta por dados do Telescopio Espacial Hubble, 3 de setembro de 2003 a 16 de janeiro de 2004, a imagem mais profunda do universo tirada em luz visivel, ilustrando o universo tal como ele era a 13 bilhoeses de anos atras.")
-  	cenadois.vai()
-'''    
-backdropcena2()
-'''
-class Cena2():
-	def __init__(self):
-		self.cena = Cena(img = linkbackdropcena)
-		#cena1 = Cena1()
-  		marilyn = Elemento (img = linkmarilyn, tit="marilyn", style = dict(right= 100, top=145, width= 115,bottom=20))
-  		marilyn.entra(self.cena)
-  		txtmarilyn = Texto(self.cena, "Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
-  		marilyn.vai = txtmarilyn.vai
-  		cenadeepspace = Cena (img = linkdeepspace)
-  		self.cena.direita = cenadeepspace
-  		cenadois = Texto(cenadeepspace, "O Hubble Ultra Deep Field uma imagem de uma pequena regiao do espaÃÂÃÂ§o,na constelaÃÂÃÂ§ao de Fornax, composta por dados do Telescopio Espacial Hubble, 3 de setembro de 2003 a 16 de janeiro de 2004, a imagem mais profunda do universo tirada em luz visivel, ilustrando o universo tal como ele era a 13 bilhoeses de anos atras.")
-  		cenadois.vai()
-  		#self.cena.esquerda = cena1
-	def vai(self, *_):
-		self.cena.vai()
+STYLE["width"] = 600
+STYLE["height"] = "600px"
 
-Cena2().vai()
+#############################################TAKE0#######################################################
+
+BACKG1 = "http://galvaoweb.com.br/escolas_01/wp-content/uploads/2017/02/01-patio.jpg"
+P1N = "https://vignette.wikia.nocookie.net/phineasferb/images/4/40/Perry_-_Imagem_Promocional_%2826%29.png"
+P2 = "https://vignette.wikia.nocookie.net/phineasferb/images/4/40/Perry_-_Imagem_Promocional_%2826%29.png"
+P3 = "https://vignette.wikia.nocookie.net/phineasferb/images/4/40/Perry_-_Imagem_Promocional_%2826%29.png"
+
+def Take0():
+    backgr =Cena(img=BACKG1)
+    narrador=  Elemento (img=P1N, style=dict (left=95, top= 100, height=100, width=100, bottom=15))
+    personagem1 =  Elemento (img=P2, style=dict (left=95, top= 100, height=100, width=100, bottom=15))
+    personagem2 =  Elemento (img=P3, style=dict (left=95, top= 100, height=100, width=100, bottom=15))
+    narrador.personagem1.personagem2.entra(backgr)
+    
+    Take.vai()
