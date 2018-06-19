c9dce41e4e6b5c388ca601b66c9401fe88bf9fb1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-19 10:41:44 -0300

    Saved julia/main.py at Timestamp: 2018-06-19 13:41:44

--- julia/main.py
+++ julia/main.py
@@ -4,17 +4,17 @@ from _spy.vitollino.main import *
 #Take 2
 IMG = {
        'background' : 'https://media-cdn.tripadvisor.com/media/photo-s/07/34/b1/37/parque-do-japao.jpg',
-       'moeda_piscante' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_2_9367638513.png?disp=inline',
-       'moeda' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_3_9501450609.png?disp=inline',
-       'seta' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/cena1_capachuva.png?disp=inline'
+       'moeda_piscante' : 'http://3.bp.blogspot.com/-xxHdoS90xwo/VR1jjtJUh1I/AAAAAAAAQAI/1Sw0A7IliUw/s1600/Coin%2B4.gif',
+       'moeda' : 'https://cdn.icon-icons.com/icons2/651/PNG/512/Icon_Business_Set_00003_A_icon-icons.com_59841.png',
+       'seta' : 'http://ap.imagensbrasil.org/images/PNG-Setadourada.png'
        }
 
 class Lago:
 	def __init__(self):
 		self.__lago = Cena(img = IMG['background']);
 		moedaPiscante = Elemento(img=IMG['moeda_piscante'], cena=self.__lago, trt="Guarda Chuva", style=dict(top=100, left=170, bottom=20, height=150, width=70))
-		moeda = Elemento(img=IMG['moeda'], cena=self.__lago, trt="Casaco", style=dict(top=150, left=220, bottom=20, height=150, width=70))
-		seta = Elemento(img=IMG['seta'], cena=self.__lago, trt="Telefone", style=dict(top=180, left=180, bottom=20, width=30))
+		moeda = Elemento(img=IMG['moeda'], cena=self.__lago, trt="Moeda Piscante", style=dict(top=150, left=220, bottom=20, height=150, width=70))
+		seta = Elemento(img=IMG['seta'], cena=self.__lago, trt="Telefone", style=dict(top=80, left=160, bottom=20, width=30))
         
 	def vai(self): self.__lago.vai();
     
