8defcd4ad33ee0a86b39298442306994d3d7fd27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-20 21:06:07 -0300

    Saved kristen/main.py at Timestamp: 2018-06-21 00:06:07

--- kristen/main.py
+++ kristen/main.py
@@ -8,7 +8,7 @@ STYLE['height'] = "600px"
 
 IMG = {
 	'background' : 'https://media-cdn.tripadvisor.com/media/photo-s/07/94/9a/9a/primeiro-portao-instalado.jpg',
-	'pixacao' : 'http://ap.imagensbrasil.org/images/PNG-Setadourada.png',
+	'pixacao' : 'https://i0.wp.com/www.heversonbarbosa.com/wp-content/uploads/2017/05/home-seta.png',
 	'portao' : 'https://activufrj.nce.ufrj.br/file/pedropeclat/1528988110963.png?disp=inline'
 }
 
@@ -18,13 +18,13 @@ class Entrada:
 
 		# Criando elementos
 		self.__entrada = Cena(img = IMG['background']);
-		portao = Elemento(img = IMG['portao'], style = dict(left= 215, top=0, width= 575,bottom=0));
+		portao = Elemento(img = IMG['portao'], style = dict(left=215, top=0, width= 575,bottom=0));
 		pixacao = Elemento(img = IMG['pixacao'], style = dict(left=0, top=95, width= 100,bottom=20));
 		aviso = Texto(self.__entrada, "O portao esta trancado");
 
 		# Funcoes
 		portao.vai = aviso.vai;
-		pixacao.vai = Lago.vai;
+		pixacao.vai = Lago().vai;
         
 		# Anexando elementos na cena
 		portao.entra(self.__entrada);
