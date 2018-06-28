e731e67637c2846578274a31d6bf556d2a1ff319
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-28 12:06:11 -0300

    Saved meredith/main.py at Timestamp: 2018-06-28 15:06:11

--- meredith/main.py
+++ meredith/main.py
@@ -1,19 +1,56 @@
-# gertrude.meredith.main.py
-from _spy.vitollino.main import Cena
+#######################################################TAKE3############################################################
+# gertrude.anastasia.main.py
+# Importando a biblioteca do Vitollino
+from _spy.vitollino.main import *
 
-linkIMG = "http://pre09.deviantart.net/3a5c/th/pre/f/2011/105/c/2/fractal_png_by_elevation_world-d3e2cex.png"
+# Configuracao da largura e altura da cena
+STYLE["width"] = 1000
+STYLE["height"] = "600px"
 
-class Cena2():
+# Criando um dicionario com as imagens
+IMG = {
+	'background' : 'https://media-cdn.tripadvisor.com/media/photo-s/07/34/b1/37/parque-do-japao.jpg',
+	'moeda_piscante' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_7_5942055903.png?disp=inline',
+	'moeda' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_9_9462807394.png?disp=inline',
+	'seta' : 'http://ap.imagensbrasil.org/images/PNG-Setadourada.png'
+}
 
-  def __init__(self):
-    self.cena2 = Cena(img = linkIMG)
-    cena1 = Cena()
-    cena1.vai = self.cena1Vai
-    self.cena2.esquerda = cena1
+# Cria a classe da cena
+class indef:
+	# Construindo a cena com os objetos e as funcoes de cada objeto
+	def __init__(self):
+        
+		# Cria a cena
+		self.__lago = Cena(img = IMG['background']);
+        
+		# Cria os objetos da cena
+		self.__moedaPiscante = Elemento(img=IMG['moeda_piscante'], style=dict(top=450, left=500, bottom=20, width=150));
+		self.__moeda = Elemento(img=IMG['moeda'], style=dict(top=450, left=700, bottom=20, width=150));
+		seta = Elemento(img=IMG['seta'], trt="Seguir em frente", style=dict(top=195, left=575, bottom=20, width=30));
 
-  def vai(self, *_):    
-    self.cena2.vai()
-  def cena1Vai(self):
-    from anastasia.main import Cena1
-    Cena1().vai
+		# Funcoes
+		self.__moedaPiscante.vai = self.__vaiMoedaPiscante;
+		self.__moeda.vai = self.__vaiMoeda;
+        
+		# Adicionando a Cena
+		self.__moedaPiscante.entra(self.__lago);
+		self.__moeda.entra(self.__lago);
+		seta.entra(self.__lago);
+
+	# Coloca a moeda piscante no inventario
+	def __vaiMoedaPiscante(self, _=0): INVENTARIO.bota(self.__moedaPiscante)      
+
+	# Coloca a moeda normal no inventario
+	def __vaiMoeda(self, _=0): INVENTARIO.bota(self.__moeda)
+
+	# Executa a cena
+	def vai(self, *_): self.__lago.vai()
+    
+    
+def go_Indef():
+  cena_Indef = indef()
+  cena_Indef.vai()
+
+if __name__ == "__main__":
+	go_Indef()
   
