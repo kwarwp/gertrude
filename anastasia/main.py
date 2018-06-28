c2df873eeb6d0456c62b4b814463c6d0c49f6dee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-28 10:53:10 -0300

    Saved anastasia/main.py at Timestamp: 2018-06-28 13:53:10

--- anastasia/main.py
+++ anastasia/main.py
@@ -25,7 +25,7 @@ class Lago:
 		self.__lago = Cena(img = IMG['background']);
         
 		# Cria os objetos da cena
-		INVENTARIO.inicia(); # Inicia o inventario
+		#INVENTARIO.inicia(); # Inicia o inventario
 		self.__moedaPiscante = Elemento(img=IMG['moeda_piscante'], style=dict(top=450, left=500, bottom=20, width=150));
 		self.__moeda = Elemento(img=IMG['moeda'], style=dict(top=450, left=700, bottom=20, width=150));
 		seta = Elemento(img=IMG['seta'], trt="Seguir em frente", style=dict(top=195, left=575, bottom=20, width=30));
