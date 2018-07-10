ebf699334a58767778bc062d633c86b8bf99e3d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-10 11:07:13 -0300

    Saved kristen/main.py at Timestamp: 2018-07-10 14:07:12

--- kristen/main.py
+++ kristen/main.py
@@ -9,7 +9,7 @@ STYLE['height'] = "600px"
 IMG = {
 	'background' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/35788257_1787410928014659_8815247365167382528_n.png?disp=inline',
 	'mapa' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/original.jpg?disp=inline',
-	'pixacao' : 'https://i0.wp.com/www.heversonbarbosa.com/wp-content/uploads/2017/05/home-seta.png',
+	'muro' : 'https://i0.wp.com/www.heversonbarbosa.com/wp-content/uploads/2017/05/home-seta.png',
 	'portao' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/portao.png?disp=inline'
 }
 
@@ -19,20 +19,20 @@ class Entrada:
 
 		# Criando elementos
 		self.__entrada = Cena(img = IMG['background']);
-		portao = Elemento(img = IMG['portao'], style = dict(left=215, top=0, width= 575,bottom=0));
-		self.__mapa = Elemento(img = IMG['mapa'], style = dict(left=240, top=300, height='80px', width=60, bottom=0, border='3px solid brown'));
-		pixacao = Elemento(img = IMG['pixacao'], style = dict(left=0, top=95, width= 100,bottom=20));
+		portao = Elemento(img = IMG['portao'], style = dict(left=227, top=15, width= 505, height='500px' ,bottom=0));
+		self.__mapa = Elemento(img = IMG['mapa'], style = dict(left=290, top=300, height='80px', width=60, bottom=0, border='3px solid brown'));
+		muro = Elemento(img = IMG['muro'], style = dict(left=0, top=95, width= 100,bottom=20));
 		aviso = Texto(self.__entrada, "O portao esta trancado");
 
 		# Funcoes
 		portao.vai = aviso.vai;
 		self.__mapa.vai = self.__vaiMapa;
-		pixacao.vai = Lago().vai;
+		muro.vai = Lago().vai;
         
 		# Anexando elementos na cena
 		portao.entra(self.__entrada);
 		self.__mapa.entra(self.__entrada);
-		pixacao.entra(self.__entrada);
+		muro.entra(self.__entrada);
 
 	def __vaiMapa(self, _=0): INVENTARIO.bota(self.__mapa)
 
