5be808a032a2e0200d5f52db21087b19c8e980d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-12 10:37:58 -0300

    Saved parisa/main.py at Timestamp: 2018-07-12 13:37:58

--- parisa/main.py
+++ parisa/main.py
@@ -11,7 +11,7 @@ LETICIA = "http://www.imagenspng.com.br/wp-content/uploads/2015/06/monica-02.png
 
 def cena3():
 
-    fundo1 = Cena(img=PATIO)
+    fundo1 = Cena(img=PATIO, direita=Entrada().vai)
     lekes = Elemento(img=LEKES, style = dict(top = 50,right = 500, height = '250px' , width = 200))
     nana = Elemento(img=NANA, style = dict(top = 300,left = 510, height = '250px' , width = 180))
     leticia = Elemento(img=LETICIA, style = dict(top = 300,left = 400, height = '250px' , width = 200))
@@ -21,13 +21,12 @@ def cena3():
     nana.entra(fundo1)
     fundo1.vai()
     
-    amgs = Texto(fundo1 ,"André- Cara, você ouviu que o Jardim Botânico está em manutenção?    Matheus- Sim, fiquei sabendo que coisas estranhas vem acontecendo lá!")
-    leticiatxt = Texto(fundo1,"Letícia- Miga, você viu que tem algo acontecendo no Jardim? Vamos lá ver?")
-    nanatxt = Texto(fundo1,"Nana- Mas você é curiosa ein, e eu também… Vamos!")
+    amgs = Texto(fundo1 ,"AndrÃ©- Cara, vocÃª ouviu que o Jardim BotÃ¢nico estÃ¡ em manutenÃ§Ã£o?    Matheus- Sim, fiquei sabendo que coisas estranhas vem acontecendo lÃ¡!")
+    leticiatxt = Texto(fundo1,"LetÃ­cia- Miga, vocÃª viu que tem algo acontecendo no Jardim? Vamos lÃ¡ ver?")
+    nanatxt = Texto(fundo1,"Nana- Mas vocÃª Ã© curiosa ein, e eu tambÃ©mâ¦ Vamos!")
 
     lekes.vai=amgs.vai
     nana.vai=nanatxt.vai
-    #leticia.vai=leticiatxt.vai
-    leticia.vai=Entrada().vai
+    leticia.vai=leticiatxt.vai
     
 cena3()
