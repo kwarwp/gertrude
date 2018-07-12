81453128bd339a7f690d0488653ecdad87c03d78
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-12 11:41:40 -0300

    Saved samantha/main.py at Timestamp: 2018-07-12 14:41:39

--- samantha/main.py
+++ samantha/main.py
@@ -24,7 +24,7 @@ class Menu:
 
 		# Criando elementos
 		self.__menu = Cena(img=IMG['patio']);
-		start = Elemento(img = IMG['start'], style = dict(left=500, top=300, width= 100,bottom=20));
+		start = Elemento(img = IMG['start'], style = dict(left=500, top=100, width= 100,bottom=20));
 		lekes = Elemento(img=IMG['lekes'], style = dict(top = 50,right = 500, height = '250px' , width = 200))
 		nana = Elemento(img=IMG['nana'], style = dict(top = 300,left = 510, height = '250px' , width = 180))
 		leticia = Elemento(img=IMG['leticia'], style = dict(top = 300,left = 400, height = '250px' , width = 200))        
@@ -32,9 +32,9 @@ class Menu:
 
 		# Funcoes
 		start.vai = Entrada().vai;
-		lekes.vai=Texto(self.__menu ,"AndrÃÂÃÂÃÂÃÂ©- Cara, vocÃÂÃÂÃÂÃÂª ouviu que o Jardim BotÃÂÃÂÃÂÃÂ¢nico estÃÂÃÂÃÂÃÂ¡ em manutenÃÂÃÂÃÂÃÂ§ÃÂÃÂÃÂÃÂ£o?    Matheus- Sim, fiquei sabendo que coisas estranhas vem acontecendo lÃÂÃÂÃÂÃÂ¡!").vai
-		nana.vai=Texto(self.__menu,"Nana- Mas vocÃÂÃÂÃÂÃÂª ÃÂÃÂÃÂÃÂ© curiosa ein, e eu tambÃÂÃÂÃÂÃÂ©mÃÂÃÂ¢ÃÂÃÂÃÂÃÂ¦ Vamos!").vai
-		leticia.vai=Texto(self.__menu,"LetÃÂÃÂÃÂÃÂ­cia- Miga, vocÃÂÃÂÃÂÃÂª viu que tem algo acontecendo no Jardim? Vamos lÃÂÃÂÃÂÃÂ¡ ver?").vai
+		lekes.vai=Texto(self.__menu ,"AndrÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©- Cara, vocÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂª ouviu que o Jardim BotÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ¢nico estÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ¡ em manutenÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ§ÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ£o?    Matheus- Sim, fiquei sabendo que coisas estranhas vem acontecendo lÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ¡!").vai
+		nana.vai=Texto(self.__menu,"Nana- Mas vocÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂª ÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ© curiosa ein, e eu tambÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©mÃÂÃÂÃÂÃÂ¢ÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ¦ Vamos!").vai
+		leticia.vai=Texto(self.__menu,"LetÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ­cia- Miga, vocÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂª viu que tem algo acontecendo no Jardim? Vamos lÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ¡ ver?").vai
         
 		# Anexando elementos na cena
 		start.entra(self.__menu);
