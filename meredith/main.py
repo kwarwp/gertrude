46093352f5a5c4ba87833e08cc2a6c363ce843ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-12 11:39:05 -0300

    Saved meredith/main.py at Timestamp: 2018-07-12 14:39:05

--- meredith/main.py
+++ meredith/main.py
@@ -59,7 +59,7 @@ class Aleia2:
 		self.__darwin = Elemento(img=IMG['darwin']);        
 
 		# Funcoes
-		self.aleia2.vai_esquerda = Aleia3().vai;        
+		self.__aleia2.vai_esquerda = Aleia3().vai;        
         
 		# Anexando elementos na cena
 		self.__bengala.entra(self.__aleia2);        
@@ -78,7 +78,7 @@ class Aleia3:
 		seta = Elemento(img=IMG['seta']);
 
 		# Funcoes
-		macaco.vai = Texto(self.__aleia3, 'Se eu fosse voces eu seguiria em frente atÃÂ© a guarita, lÃÂ¡ voces vao poder se alimentar e recuperar o folego').vai;
+		macaco.vai = Texto(self.__aleia3, 'Se eu fosse voces eu seguiria em frente atÃÂÃÂ© a guarita, lÃÂÃÂ¡ voces vao poder se alimentar e recuperar o folego').vai;
 		seta.vai = Guarita().vai;        
         
 		# Anexando elementos na cena
