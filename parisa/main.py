4d7d15346314cf5d0058e6b9ed7bf01e7bfe4a4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-12 10:09:17 -0300

    Created parisa/main.py at Timestamp: 2018-07-12 13:09:17

---
+++ parisa/main.py
@@ -0,0 +1,20 @@
+from _spy.vitollino.main import Cena, STYLE, Texto, Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+
+STYLE['width'] = 700
+STYLE["height"] = "600px"
+
+PATIO= "https://activufrj.nce.ufrj.br/file/GamesInteligentesII/20180710_111303.jpg?disp=inline"
+LEKES= "http://3.bp.blogspot.com/-_JxmohXYIHI/Ud89A7TrTHI/AAAAAAAABQY/RGp-9STuc2c/s1600/pooh7.png"
+
+def cena3():
+
+    fundo1 = Cena(img=PATIO)
+    lekes = Elemento(img=LEKES)
+
+    lekes.entra(fundo1)
+    fundo1.vai()
+    amgs = Texto(fundo1 ,"André- Cara, você ouviu que o Jardim Botânico está em manutenção?    Matheus- Sim, fiquei sabendo que coisas estranhas vem acontecendo lá!")
+    lekes.vai=amgs.vai
+    
+cena3()
