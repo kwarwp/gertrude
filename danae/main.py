e723864e089c8c165000170fa68814f705ab5dfe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 15:27:28 -0300

    Saved danae/main.py at Timestamp: 2018-07-13 18:27:27

--- danae/main.py
+++ danae/main.py
@@ -7,7 +7,8 @@ STYLE['height'] = "600px"
 
 IMG = {
 	'background' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/36410919_1803508649738220_8839554797003603968_n.png?disp=inline',
-	'guarita': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/guarita.png?disp=inline',
+	'background2' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/36410919_1803508649738220_8839554797003603968_n.png?disp=inline',    
+	'cabine': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/guarita.png?disp=inline',
 	'mapa' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/original.jpg?disp=inline',
 	'pixacao' : 'https://i0.wp.com/www.heversonbarbosa.com/wp-content/uploads/2017/05/home-seta.png',
 	'portao' : 'https://activufrj.nce.ufrj.br/file/pedropeclat/1528988110963.png?disp=inline'
@@ -19,14 +20,30 @@ class Guarita:
 
 		# Criando elementos
 		self.__guarita = Cena(img = IMG['background']);
-		cabine = Elemento(img=IMG['guarita'], style=dict(top=0,bottom=20, width=230, left=57));
+		cabine = Elemento(img=IMG['cabine'], style=dict(top=0,bottom=20, width=230, left=57));
 
 		# Funcoes
+		cabine.vai = Guarita2();
         
 		# Anexando elementos na cena
 		cabine.entra(self.__guarita);
 
 	def vai(self, *_): self.__guarita.vai();
+
+class Guarita2:
+
+	def __init__(self):
+
+		# Criando elementos
+		self.__guarita2 = Cena(img = IMG['background2']);
+		cabine = Elemento(img=IMG['cabine'], style=dict(top=0,bottom=20, width=230, left=57));
+
+		# Funcoes
+        
+		# Anexando elementos na cena
+		cabine.entra(self.__guarita);
     
+def vai(self, *_): self.__guarita2.vai();
+
 # Guarita().vai();
 # Guarita2().vai();
