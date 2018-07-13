9867ea92e8623c3164dfbd01f94d84f9c64a6ae9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-12 21:09:01 -0300

    Saved danae/main.py at Timestamp: 2018-07-13 00:09:00

--- danae/main.py
+++ danae/main.py
@@ -7,6 +7,7 @@ STYLE['height'] = "600px"
 
 IMG = {
 	'background' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/36410919_1803508649738220_8839554797003603968_n.png?disp=inline',
+	'guarita': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/guarita.png?disp=inline',
 	'mapa' : 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/original.jpg?disp=inline',
 	'pixacao' : 'https://i0.wp.com/www.heversonbarbosa.com/wp-content/uploads/2017/05/home-seta.png',
 	'portao' : 'https://activufrj.nce.ufrj.br/file/pedropeclat/1528988110963.png?disp=inline'
@@ -18,11 +19,13 @@ class Guarita:
 
 		# Criando elementos
 		self.__guarita = Cena(img = IMG['background']);
+		cabine = Elemento(img=IMG['guarita'], style=dict(top=0,bottom=20, width=230, left=57));
 
 		# Funcoes
         
 		# Anexando elementos na cena
+		cabine.entra(self.__guarita);
 
 	def vai(self, *_): self.__guarita.vai();
     
-# Guarita().vai()
+Guarita().vai()
