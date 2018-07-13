a01907e54b49aaad27da4924e20cffd06270c81c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 15:29:38 -0300

    Saved meredith/main.py at Timestamp: 2018-07-13 18:29:37

--- meredith/main.py
+++ meredith/main.py
@@ -2,7 +2,7 @@
 # gertrude.meredith.main.py
 # Importando a biblioteca do Vitollino
 from _spy.vitollino.main import *
-from danae.main import Guarita
+from danae.main import Guarita1
 
 # Configuracao da largura e altura da cena
 STYLE["width"] = 1000
@@ -10,9 +10,9 @@ STYLE["height"] = "600px"
 
 # Criando um dicionario com as imagens
 IMG = {
-	'aleia1': 'http://ebendinger.jbrj.gov.br/fotos/75.jpg',
-	'aleia2': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/36410919_1803508649738220_8839554797003603968_n.png?disp=inline',    
-	'aleia3': 'http://ebendinger.jbrj.gov.br/fotos/75.jpg',    
+	'background1': 'http://ebendinger.jbrj.gov.br/fotos/75.jpg',
+	'background2': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/36410919_1803508649738220_8839554797003603968_n.png?disp=inline',    
+	'background3': 'http://ebendinger.jbrj.gov.br/fotos/75.jpg',    
 	'passarinho': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/Bird2.png?disp=inline',
 	'ninho': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_16_2807468112.png?disp=inline',
 	'maca': 'https://activufrj.nce.ufrj.br/file/GamesInteligentesII/imageedit_34_5062328837.png?disp=inline',    
@@ -28,12 +28,12 @@ SOUNDS = {
 }
 
 # Cria a classe da cena
-class Aleia:
+class Aleia1:
 	# Construindo a cena com os objetos e as funcoes de cada objeto
 	def __init__(self):
         
 		# Cria a cena
-		self.__aleia = Cena(img = IMG['aleia1']);
+		self.__aleia1 = Cena(img = IMG['background1']);
         
 		# Cria os objetos da cena
 		self.__passarinho = Elemento(img=IMG['passarinho'], style=dict(top=430, left=470, bottom=20, width=100));
@@ -45,28 +45,28 @@ class Aleia:
 		seta.vai = Aleia2().vai;
         
 		# Adicionando a Cena
-		self.__passarinho.entra(self.__aleia);
-		self.__ninho.entra(self.__aleia);
-		seta.entra(self.__aleia);
+		self.__passarinho.entra(self.__aleia1);
+		self.__ninho.entra(self.__aleia1);
+		seta.entra(self.__aleia1);
 
 	# Coloca o passarinho no ninho    
 	def __saiPassarinho(self, _=0): 
 		self.__passarinho.elt.style = dict(display= 'none');
 		self.__ninho.elt.style = dict(display= 'none');
-		ninho = Elemento(img=IMG['seta'], cena=self.__aleia, style=dict(top=100, left=600, bottom=20, width=150));
+		ninho = Elemento(img=IMG['seta'], cena=self.__aleia1, style=dict(top=100, left=600, bottom=20, width=150));
 		maca = Elemento(img=IMG['maca'], style=dict(top=100, left=600, bottom=20, width=150));
-		Texto(self.__aleia, 'Obrigado por colocar meu filho de volta no ninho como recompensa toma aqui essa fruta').vai()
+		Texto(self.__aleia1, 'Obrigado por colocar meu filho de volta no ninho como recompensa toma aqui essa fruta').vai()
 		INVENTARIO.bota(maca)        
 
 	# Executa a cena
-	def vai(self, *_): self.__aleia.vai()
+	def vai(self, *_): self.__aleia1.vai()
 
 class Aleia2:
 
 	def __init__(self):
          
 		# Criando elementos
-		self.__aleia2 = Cena(img = IMG['aleia2']);
+		self.__aleia2 = Cena(img = IMG['background2']);
                 
 		self.__pedra = Elemento(img=IMG['pedra'], style=dict(left=520, width=800, top=250, bottom=20));      
 		self.__bengala = Elemento(img=IMG['bengala'], style=dict(left=670, top=270, bottom=20, width=500));        
@@ -93,13 +93,13 @@ class Aleia3:
 	def __init__(self):
          
 		# Criando elementos
-		self.__aleia3 = Cena(img = IMG['aleia3']);
+		self.__aleia3 = Cena(img = IMG['background3']);
 		macaco = Elemento(img=IMG['macaco'], style=dict(top=300, left=300, bottom=20, width=150));
 		seta = Elemento(img=IMG['seta']);
 
 		# Funcoes
 		macaco.vai = Texto(self.__aleia3, 'Se eu fosse voces eu seguiria em frente ate voces vao poder se alimentar e recuperar o folego').vai;
-		seta.vai = Guarita().vai;        
+		seta.vai = Guarita1().vai;        
         
 		# Anexando elementos na cena
 		macaco.entra(self.__aleia3);
@@ -107,6 +107,6 @@ class Aleia3:
 
 	def vai(self, *_): self.__aleia3.vai();
 
-# Aleia().vai()
+# Aleia1().vai()
 # Aleia2().vai()
 # Aleia3().vai()
