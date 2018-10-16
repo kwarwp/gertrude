5571129c1e2e7ca1f668263533d03f6c89a0a1da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-16 15:20:42 -0300

    Created samantha/__log__.py at Timestamp: 2018-10-16 18:20:42

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 16 2018 15:20:38.796 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 26
+  self.__menu = Cena(img=IMG['patio']);
+  ^
+IndentationError: expected an indented block
+'''},
