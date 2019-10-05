8f3d8c1d99c7988c5a955ebc63fd0cd0ee55504c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-05 16:50:06 -0300

    Saved samantha/__log__.py @2019-10-05 19:50

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -42,3 +42,21 @@ NameError: name 'start' is not defined
     start.vai = Entrada().vai;
 NameError: name 'start' is not defined
 '''},
+{'date': 'Sat Oct 05 2019 16:48:55.655 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    class Menu:
+  module <module> line 34
+    start.vai = Entrada().vai;
+NameError: name 'start' is not defined
+'''},
