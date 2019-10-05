a0ab8fc9c0012e56a3c4e5d0f3a7a582573f0254
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-05 11:40:09 -0300

    Created sara/__log__.py @2019-10-05 14:40

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Oct 05 2019 11:38:58.35 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 25
+  if escolha: pass#self.cena.direita = #sucesso;
+  ^
+IndentationError: unexpected indent
+'''},
