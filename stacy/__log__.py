be214fe8c3d2cc3ce42b4a670c173c05713fdf8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-05 11:31:32 -0300

    Created stacy/__log__.py @2019-10-05 14:31

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Oct 05 2019 11:30:21.316 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  self.__personagem = Elemento(img = img, trt = nome, style = posicao, vai = texto.vai);
+  ^
+IndentationError: unexpected indent
+'''},
