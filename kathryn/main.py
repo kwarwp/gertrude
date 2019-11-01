dc764b626bc2758ff1e67f4a66bf62de395f57b8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:10:13 -0300

    Saved kathryn/main.py @2019-11-01 17:10

--- kathryn/main.py
+++ kathryn/main.py
@@ -8,3 +8,13 @@ class sambinha():
     lillapizinho.entra(midas)
     midas.vai()
 sambinha()
+from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+LILLAPIZINHO="https://a-static.mlcdn.com.br/1500x1500/boneco-roblox-phantom-forces-ghost-brinquedos-chocolate/plazagames/1297/54d22d532df671e0503be50c285c0739.jpg"
+MIDAS="https://images-eu.ssl-images-amazon.com/images/I/913LOf4vO3L.png"
+class sambinha():
+    lillapizinho= Elemento(img=LILLAPIZINHO)
+    midas= Cena(img=MIDAS)
+    lillapizinho.entra(midas)
+    midas.vai()
+sambinha()
