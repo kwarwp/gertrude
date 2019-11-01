e7af866211b2a70766e75907f16735626a5dcbe2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:08:51 -0300

    Created kathryn/__log__.py @2019-11-01 17:08

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 14:08:42.305 GMt-0200 (Horário brasileiro de verão) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  lillapizinho.entra(no midas)
+                         ^
+SyntaxError: invalid syntax
+'''},
