9ebe07f1350e6d0e5c6a87639ab93965142fc2b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 09:30:30 -0300

    Saved courtney/__log__.py @2022-07-14 12:30

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -97,3 +97,25 @@ AttributeError: 'str' object has no attribute 'nome'
     from soraya.main import Bloco
 ImportError: cannot import name 'Bloco'
 '''},
+{'date': 'Thu Jul 14 2022 09:30:30.369 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from soraya.main import Bloco
+  module soraya.main line 39
+    Mapa1()
+  module soraya.main line 34
+    loc.entra(MAPA0)
+  module _spy.vitollino.main line 482
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'str' object has no attribute 'nome'
+'''},
